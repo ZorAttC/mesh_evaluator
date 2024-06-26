5a5494667f6edbf6179d44462a11d8569abec297
ZorAttC <1720049199@qq.com>
2024-06-26 13:25:46 +0800

    A evaluator tool which supports mesh2mesh and mesh2pointcloud evaluation

---
+++ config.py
@@ -0,0 +1,14 @@
+########################################### MaiCity Dataset ###########################################
+dataset_name = "maicity_01_"
+c_gt_pcd_path = "/home/zoratt/ImMesh_output/kitti04_raw.ply"
+c_pred_mesh_path = "/home/zoratt/ImMesh_output/kitti04_raw.ply"
+method_name = "ours_new"
+# evaluation results output file
+base_output_folder = "./experiments/evaluation/"
+output_csv_path =  method_name + "_eval.csv"
+# evaluation parameters
+# For MaiCity
+down_sample_vox = -1 #-1 means no downsample,to make sure the same density as the ground truth
+dist_thre = 0.2
+truncation_dist_acc = 2.0 
+truncation_dist_com = 2.0
---
+++ evaluator.py
@@ -0,0 +1,68 @@
+import csv
+from eval_utils import eval_mesh
+from eval_utils import eval_mesh2mesh
+import argh
+from config import *
+
+
+
+# pred_mesh_path = "xxx/baseline/vdb_fusion_xxx.ply"
+# method_name = "vdb_fusion_xxx"
+
+# pred_mesh_path = "xxx/baseline/puma_xxx.ply"
+# method_name = "puma_xxx"
+
+def main(
+        pred_mesh_path:str=c_pred_mesh_path,
+        gt_ply_path:str=c_gt_pcd_path,
+        eval_mode:int=0,#0 for mesh2mesh,1 for mesh2pointcloud
+):
+
+    if eval_mode == 0:
+        print("mesh to mesh evaluation")
+        eval_metric = eval_mesh2mesh(pred_mesh_path, gt_ply_path, down_sample_res=down_sample_vox, threshold=dist_thre, 
+                                truncation_acc = truncation_dist_acc, truncation_com = truncation_dist_com, gt_bbx_mask_on = True) 
+
+        print(eval_metric)
+
+        evals = [eval_metric]
+
+        csv_columns = ['MAE_accuracy (m)', 'MAE_completeness (m)', 'Chamfer_L1 (m)', 'Chamfer_L2 (m)', \
+                'Precision [Accuracy] (%)', 'Recall [Completeness] (%)', 'F-score (%)', 'Spacing (m)', \
+                'Inlier_threshold (m)', 'Outlier_truncation_acc (m)', 'Outlier_truncation_com (m)']
+
+        try:
+            with open(output_csv_path, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in evals:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
+    elif eval_mode==1:
+        print("mesh to pointcloud evaluation")
+        #TODO:translation not implemented
+        eval_metric = eval_mesh(pred_mesh_path, gt_ply_path, down_sample_res=down_sample_vox, threshold=dist_thre, 
+                                truncation_acc = truncation_dist_acc, truncation_com = truncation_dist_com, gt_bbx_mask_on = True) 
+
+        print(eval_metric)
+
+        evals = [eval_metric]
+
+        csv_columns = ['MAE_accuracy (m)', 'MAE_completeness (m)', 'Chamfer_L1 (m)', 'Chamfer_L2 (m)', \
+                'Precision [Accuracy] (%)', 'Recall [Completeness] (%)', 'F-score (%)', 'Spacing (m)', \
+                'Inlier_threshold (m)', 'Outlier_truncation_acc (m)', 'Outlier_truncation_com (m)']
+
+        try:
+            with open(output_csv_path, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in evals:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
+
+
+if __name__ == "__main__":
+    argh.dispatch_command(main)
+
